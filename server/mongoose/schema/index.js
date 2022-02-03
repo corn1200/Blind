a405582fa56e445f402004c13079ac862a28abe1
corn1200 <corn120033@gmail.com>
2022-02-04 01:42:43 +0900

    index.js에서 데이터 모델 모듈을 사용하도록 수정합니다

---
+++ server/mongoose/schema/index.js
@@ -0,0 +1,15 @@
+const Article = require("./article"); // 게시글
+const Board = require("./board"); // 게시판
+const Comment = require("./comment"); // 댓글
+const Company = require("./company"); // 회사
+const Reply = require("./reply"); // 대댓글
+const User = require("./user"); // 유저
+
+module.exports = {
+  Article,
+  Board,
+  Comment,
+  Company,
+  Reply,
+  User,
+};
