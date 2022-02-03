10f5b0fb0d3d406d673c7e8e04aaab769dbb05cd
corn1200 <corn120033@gmail.com>
2022-02-04 01:40:46 +0900

    댓글 데이터 모델에 작성자를 추가합니다

--- server/mongoose/schema/comment.js
+++ server/mongoose/schema/comment.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Comment = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User" },
   article: { type: Schema.Types.ObjectId, ref: "Article" },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, required: true },
