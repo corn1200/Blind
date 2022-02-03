5d17c46f6bc553608f7645f13e6cf972229dff77
corn1200 <corn120033@gmail.com>
2022-02-03 21:09:11 +0900

    회사 데이터 모델을 추가합니다

---
+++ server/mongoose/schema/company.js
@@ -0,0 +1,15 @@
+// 회사
+const mongoose = require("mongoose");
+
+const Company = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  createAt: { type: Date, default: Date.now, required: true },
+  imgAddress: { type: String, default: null },
+  reviewScore: { type: Number, default: 3, required: true },
+  realtimeScore: { type: Number, default: 0 },
+  // TODO: 자주 바뀌는 데이터는 따로 빼두는게 효율적입니다
+  // 계속 IO가 발생하는 실시간 검색 데이터 및 
+  // 리뷰 데이터는 스키마를 분리해야 합니다
+});
+
+module.exports = Company;
