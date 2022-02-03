0a91a8edde6a417e323de8132ed27214d7506fe7
corn1200 <corn120033@gmail.com>
2022-02-04 05:56:45 +0900

    회사 데이터 모델 필드명 변경

--- server/mongoose/schema/company.js
+++ server/mongoose/schema/company.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Company = new Schema({
-  title: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   createAt: { type: Date, default: Date.now, required: true },
   imgAddress: { type: String, default: null },
   reviewScore: { type: Number, default: 3, required: true },
