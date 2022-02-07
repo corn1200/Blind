c15bb9f30ade229caa0b111443f2f0b405ed5355
corn1200 <corn120033@gmail.com>
2022-02-07 21:35:10 +0900

    회사 스키마의 필드 기본 값을 수정합니다

--- server/mongoose/schema/company.js
+++ server/mongoose/schema/company.js
@@ -6,7 +6,7 @@ const Company = new Schema({
   name: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now, required: true },
   imgAddress: { type: String, default: null },
-  reviewScore: { type: Number, default: 3, required: true },
+  reviewScore: { type: Number, default: 0, required: true },
   realtimeScore: { type: Number, default: 0 },
   // TODO: 자주 바뀌는 데이터는 따로 빼두는게 효율적입니다
   // 계속 IO가 발생하는 실시간 검색 데이터 및 
